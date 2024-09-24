9a5175217f31c0948247b75a8a922b50906939fe
Karol-Alb <Albertusiak-K@ulster.ac.uk>
2024-09-24 15:25:42 +0100

    created a calculator

--- Calculator2/Program.cs
+++ Calculator2/Program.cs
@@ -1,8 +1,56 @@
-﻿Console.WriteLine(" Type in the first number followed by the Enter key");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+﻿
 
-Console.WriteLine(" Tpe the second number, and then press Enter ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+CalculatorApp();
+void CalculatorApp()
+{
+    int firstNumber = 0;
+    int secondNumber = 0;
+    int result = 0;
+    int choice = 0;
 
-int result = firstNumber + secondNumber;
-Console.WriteLine(" Adding {0} and {1} give the answer {2}", firstNumber, secondNumber, result);
+
+    Console.WriteLine(" Type in the first number, and then press Enter");
+    firstNumber = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(" Type the second number, and then press Enter");
+    secondNumber = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(" Choose an option from the following list:");
+    Console.WriteLine(" 1 - Add");
+    Console.WriteLine(" 2 - Subtract");
+    Console.WriteLine(" 3 - Divide");
+    Console.WriteLine(" 4 - Multiply");
+    choice = Convert.ToInt32(Console.ReadLine());
+
+    if (choice == 1)
+    {
+        result = firstNumber + secondNumber;
+        Console.WriteLine($"Adding {firstNumber} and {secondNumber} equals {result}");
+    }
+    else if (choice == 2)
+    {
+        result = firstNumber - secondNumber;
+        Console.WriteLine($"Subtracting {firstNumber} from {secondNumber} equals {result}");
+    }
+    else if (choice == 3)
+    {
+        result = firstNumber / secondNumber;
+        Console.WriteLine($"Dividing {firstNumber} by {secondNumber} equals {result}");
+
+    }
+    else if (choice == 4)
+    {
+        result = firstNumber * secondNumber;
+        Console.WriteLine($"Multiplying {firstNumber} by {secondNumber} equals {result}");
+
+    }
+    else
+    {
+        Console.WriteLine("You did not select a valid number between 1-4");
+    }
+
+
+        
+
+    
+}
